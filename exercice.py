c11a70fbaa16cbd45815ed832463aa6d98960c23
Justin Lachapelle <46169956+justinlachap@users.noreply.github.com>
2020-09-10 18:04:13 -0400

    Update exercice.py

--- exercice.py
+++ exercice.py
@@ -2,13 +2,9 @@
 # -*- coding: utf-8 -*-
 def majuscule(mot):
     resultat = ''
-    index = 0
     for lettre in mot:
-        if index == 0:
-            resultat += chr(ord(lettre)-32)
-        else:
-            resultat += lettre
-        index += 1 
+       resultat += chr(ord(lettre)-32)
+        
     return resultat
 
 
